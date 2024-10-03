ae58b1647073d5cc5c6f33237182714f43e443b5
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-10-03 12:33:04 +0200

    Modificando CreateProductEndpoint + CreateProductHandler e instalando la paquete Carter

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,13 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    // records
+
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id); 
+
+    // ENDPOINTS 
+    // GET, POST, PUT, DELETE
+    // Para organizar los endpoints más estructurados utilizamos una libreria que se llama Carter. 
     public class CreateProductEndpoint
     {
     }
--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -3,20 +3,35 @@
 /// 1 - Instalar la paquete MediatR (para una arquitectura vertical)
 /// 2 - Usando public record, instanciar las propiedades del producto y estas propiedades heredan de IRequest<CreateProductResult> y encapusaladas por CreateProductCommand
 /// 3 - Usando public record, instanciar el Id a partir de CreateProductResult
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 
 {
     // Instanciando los datos desde el modelo de Producto
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
     // Un método interno
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // Create a product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+            // Save to the database
+
+
+            // return CreateProductResult result 
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
